45ecf8d5b600ce063e6319ed4a805aed229e9e92
Pauls-ux9 <paulsangacha778@gmail.com>
2025-03-26 12:37:01 -0500

    cambios

--- src/Actividad/Modelo.java
+++ src/Actividad/Modelo.java
@@ -9,24 +9,24 @@ import java.util.Arrays;
 public class Modelo {
    private int[] vector;
 
-    // Constructor para definir tamaño del vector
+    
     public Modelo(int tamaño) {
         vector = new int[tamaño];
     }
 
-    // Método para ingresar datos al vector
+    
     public void setDato(int indice, int valor) {
         if (indice >= 0 && indice < vector.length) {
             vector[indice] = valor;
         }
     }
 
-    // Método para encontrar el mayor número
+    
     public int getMayor() {
         return Arrays.stream(vector).max().orElse(Integer.MIN_VALUE);
     }
 
-    // Método para obtener todo el vector como String
+    
     public String[] getDatos() {
         return Arrays.stream(vector).mapToObj(String::valueOf).toArray(String[]::new);
     }
